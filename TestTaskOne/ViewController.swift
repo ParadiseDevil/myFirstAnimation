04f6702101b1e98b03e00f4caca5a3b7022249b8
Damir Shantin <evilberrydd@gmail.com>
2023-08-21 17:43:50 +0300

    Initial Commit

---
+++ TestTaskOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskOne
+//
+//  Created by Дамир Шаймуханбетов on 21.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
